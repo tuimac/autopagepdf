9f91bdf1421390fa22fb47702e98b3f3e630b946
tuimac <tuimac.devadm01@gmail.com>
2022-12-30 13:05:03 +0900

    fixed bugs which skip the invalid url

--- src/AutoPagePdf.py
+++ src/AutoPagePdf.py
@@ -1,28 +1,70 @@
 import openpyxl
 import json
-import time
-import os
 import sys
-import traceback
 import platform
-import zipfile
 import urllib.request
+import logging.config
+from time import sleep
+from zipfile import ZipFile
+from traceback import print_exc, format_exc
+from os import chmod, path, _exit, getcwd
+from logging import getLogger
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-CONF = dict()
 DRIVER_MAP = {
     "Linux": "chromedriver_linux64.zip",
     "Darwin": "chromedriver_mac64.zip",
     "Windows": "chromedriver_win32.zip"
 }
+CONFIG_PATH = 'config.json'
 
 def load_conf_file():
-    with open('config.json', 'r', encoding='utf-8_sig') as config:
-        global CONF
-        CONF = json.load(config)
+    global logger
+    global CONF
+    try:
+        # Loading configration file
+        if path.exists('config.json') is False:
+            raise FileNotFoundError
+        with open('config.json', 'r', encoding='utf-8_sig') as config:
+            CONF = json.load(config)
+
+        # Define logger
+        logging.config.dictConfig(
+            {
+                "version": 1,
+                "disable_existing_loggers": "True",
+                "formatters": {
+                    "file": {
+                        "format": "[%(levelname)s] - %(asctime)s - Source log file: %(pathname)s line: %(lineno)s %(message)s"
+                    }
+                },
+                "handlers": {
+                    "file": {
+                        "level": "INFO",
+                        "class": "logging.handlers.RotatingFileHandler",
+                        "filename": CONF['LOG_FILE_PATH'],
+                        "maxBytes": 10485760,
+                        "formatter": "file"
+                    }
+                },
+                "loggers": {
+                    "main": {
+                        "handlers": ["file"],
+                        "level": "INFO",
+                        "propagate": "True"
+                    }
+                }
+            }
+        )
+        logger = getLogger('main')
+    except:
+        print_exc()
+        with open(CONF['LOG_FILE_PATH'], 'a', encoding='utf-8_sig') as f:
+            f.write(format_exc())
+        _exit(1)
 
 def import_excel() -> dict:
     data = dict()
@@ -40,16 +82,17 @@ def import_excel() -> dict:
             row += 1
     return data
 
+
 def __check_chromedriver_path(os_type) -> str:
     if os_type == 'Linux' or os_type == 'Darwin':
         driver_file_path = CONF['DRIVER_DIR']+ '/chromedriver'
-        if os.path.exists(driver_file_path) is True:
+        if path.exists(driver_file_path) is True:
             return driver_file_path
         else:
             return ''
     elif os_type == 'Windows':
         driver_file_path = CONF['DRIVER_DIR'] + '/chromedriver.exe'
-        if os.path.exists(driver_file_path) is True:
+        if path.exists(driver_file_path) is True:
             return driver_file_path
         else:
             return ''
@@ -69,47 +112,38 @@ def __check_exclude_word(url):
                 return True
 
 def download_chromedriver() -> str:
-    try:
-        os_type = platform.system()
-        
-        # Confirm if there is Chrome driver under the DRIVER_DIR
-        chromedriver_path = __check_chromedriver_path(os_type)
-        if chromedriver_path != '':
-            return chromedriver_path
-
-        # Get the latest version information for Chrome on this machine
-        main_chrome_version = CONF['CHROME_VERSION'].split('.')[0]
-        handler = urllib.request.urlopen(
-            'https://chromedriver.storage.googleapis.com/LATEST_RELEASE_' + main_chrome_version
-        )
-        latest_driver_version = handler.read().decode()
-        handler.close()
-        
-        # Download the valid version Chrome driver for this machine
-        download_path = CONF['DRIVER_DIR'] + '/' + DRIVER_MAP[os_type]
-        handler = urllib.request.urlretrieve(
-            'https://chromedriver.storage.googleapis.com/' + latest_driver_version + '/' + DRIVER_MAP[os_type],
-            download_path
-        )
-        with zipfile.ZipFile(download_path) as zip_handler:
-            zip_handler.extractall(CONF['DRIVER_DIR'])
-        os.remove(download_path)
+    os_type = platform.system()
     
-        # Confirm if there is Chrome driver under the DRIVER_DIR
-        chromedriver_path = __check_chromedriver_path(os_type)
-        if chromedriver_path == '':
-            raise KeyError
-        else:
-            os.chmod(chromedriver_path, 0o755)
-            return chromedriver_path
+    # Confirm if there is Chrome driver under the DRIVER_DIR
+    chromedriver_path = __check_chromedriver_path(os_type)
+    if chromedriver_path != '':
+        return chromedriver_path
 
-    except KeyError as e:
-        traceback.print_exc()
-        print('There is no support for this OS.(' + os_type + ')', file=sys.stderr)
-        os._exit(1)
-    except:
-        traceback.print_exc()
-        os._exit(1)
+    # Get the latest version information for Chrome on this machine
+    main_chrome_version = CONF['CHROME_VERSION'].split('.')[0]
+    handler = urllib.request.urlopen(
+        'https://chromedriver.storage.googleapis.com/LATEST_RELEASE_' + main_chrome_version
+    )
+    latest_driver_version = handler.read().decode()
+    handler.close()
+    
+    # Download the valid version Chrome driver for this machine
+    download_path = CONF['DRIVER_DIR'] + '/' + DRIVER_MAP[os_type]
+    handler = urllib.request.urlretrieve(
+        'https://chromedriver.storage.googleapis.com/' + latest_driver_version + '/' + DRIVER_MAP[os_type],
+        download_path
+    )
+    with ZipFile(download_path) as zip_handler:
+        zip_handler.extractall(CONF['DRIVER_DIR'])
+    remove(download_path)
+
+    # Confirm if there is Chrome driver under the DRIVER_DIR
+    chromedriver_path = __check_chromedriver_path(os_type)
+    if chromedriver_path == '':
+        raise KeyError
+    else:
+        chmod(chromedriver_path, 0o755)
+        return chromedriver_path
 
 def create_pdf(data, driver_path):
     # Setup Chrome options for prinr page as PDF
@@ -138,7 +172,7 @@ def create_pdf(data, driver_path):
     
     prefs = {
         'printing.print_preview_sticky_settings.appState': json.dumps(printer_config),
-        'download.default_directory': os.getcwd(),
+        'download.default_directory': getcwd(),
         'download.open_pdf_in_system_reader': False,
         'download.prompt_for_download': False,
         'plugins.always_open_pdf_externally': True,
@@ -152,19 +186,23 @@ def create_pdf(data, driver_path):
 
     # Create PDF from each url website
     driver = webdriver.Chrome(executable_path=driver_path, options=chrome_option)
-    try:
-        for key in data:
+
+    for key in data:
+        try:
             url = data[key]
             if __check_exclude_word(url) is False:
                 continue
-            driver.implicitly_wait(10)
             driver.get(url)
             WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
             driver.execute_script('document.title="' + str(key) + '";window.print();')
-            time.sleep(CONF['INTERVAL'])
-        driver.quit()
-    except:
-        pass
+            sleep(CONF['INTERVAL'])
+        except urllib.error.HTTPError:
+            print_exc()
+            logger.error(url + ' is invalid url. So cannot reachable.')
+        except:
+            print_exc()
+            logger.error(format_exc())
+    driver.quit()
 
 if __name__ == '__main__':
     try:
@@ -173,5 +211,5 @@ if __name__ == '__main__':
         driver_path = download_chromedriver()
         create_pdf(data, driver_path)
     except:
-        with open(CONF['LOG_FILE_PATH'], 'a', encoding='utf-8_sig') as f:
-            f.write(traceback.format_exc())
+        print_exc()
+        logger.error(format_exc())
